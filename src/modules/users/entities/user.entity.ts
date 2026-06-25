71b359085d9c2e4edb8211cf8c83854d772a169d
ronni2030 <rav.villa@yavirac.edu.ec>
2026-06-25 16:09:02 -0500

    Cambio de migraciones y agregacion de columnas a la tabla

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,7 +1,14 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id!:number;
+    @Column()
+    name!:string;
+    @Column()
+    mail!:string;
+    @Column()
+    password!:string;
+
 }
